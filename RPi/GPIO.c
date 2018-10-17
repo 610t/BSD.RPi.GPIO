3e50558589f0c243d10dcce1c98d882d5f5fd9a9
Takeshi MUTOH <takeshi.mutoh@gmail.com>
2018-10-17 16:47:31 +0900

    Add FreeBSD support.

--- RPi/GPIO.c
+++ RPi/GPIO.c
@@ -7,8 +7,12 @@
 #include "Python.h"
 
 #include <sys/types.h>
+#ifdef __FreeBSD__
+#include <libgpio.h>
+#elif defined __NetBSD__
 #include <sys/gpio.h>
 #include <sys/ioctl.h>
+#endif
 
 #define BCM	0
 #define OUT	0
@@ -18,7 +22,12 @@ static PyObject *bcm;
 static PyObject *out;
 static PyObject *in;
 
+#ifdef __FreeBSD__
+gpio_handle_t gpio_handle=(gpio_handle_t)NULL;
+#elif defined __NetBSD__
 #define DEV	"/dev/gpio0"
+#endif
+
 int fd;
 
 PyObject* GPIO_output(PyObject* self, PyObject* args)
@@ -34,11 +43,15 @@ PyObject* GPIO_output(PyObject* self, PyObject* args)
 	if(v>1) v=1;
 	if(v<0) v=0;
 
+#ifdef __FreeBSD__
+	gpio_pin_set(gpio_handle,pin,v);
+#elif defined __NetBSD__
 	memset(&req, 0, sizeof(req));
 	req.gp_pin = pin;
 	req.gp_value = v;
 	if(ioctl(fd, GPIOWRITE, &req)==-1)
 		err(EXIT_FAILURE, "GPIOWRITE");
+#endif
 
 	return Py_BuildValue("");
 }
@@ -58,6 +71,7 @@ PyObject* GPIO_setwarnings(PyObject* self, PyObject* args)
 	int f;
 
 	if (!PyArg_ParseTuple(args, "i", &f))
+		return NULL;
 	printf("setwarnings: %d\n",f);
 	return Py_BuildValue("");
 }
@@ -65,12 +79,29 @@ PyObject* GPIO_setwarnings(PyObject* self, PyObject* args)
 PyObject* GPIO_setup(PyObject* self, PyObject* args)
 {
 	int pin,mode;
+#ifdef __FreeBSD__
+	gpio_config_t config;
+#endif
 
 	if (!PyArg_ParseTuple(args, "ii", &pin, &mode))
+	  return NULL;
 	printf("setup: %d %d\n",pin,mode);
 
+#ifdef __FreeBSD__
+	if(gpio_handle==(gpio_handle_t)NULL)
+		gpio_handle=gpio_open(0);
+
+	config.g_pin=pin;
+	if(mode==OUT)
+	  config.g_flags=GPIO_PIN_OUTPUT;
+	else if(mode==IN)
+	  config.g_flags=GPIO_PIN_INPUT;
+
+	gpio_pin_set_flags(gpio_handle, &config);
+#elif defined __NetBSD__
 	if((fd=open(DEV,O_RDWR)) == -1)
 		err(EXIT_FAILURE, "%s", DEV);
+#endif
 
 	return Py_BuildValue("");
 }
